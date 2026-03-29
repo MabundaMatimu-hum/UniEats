107229c3e353a7cfc7291eb99be4959a54db374c
Matimu Mabunda <yourgithubemail@gmail.com>
2026-03-29 13:00:53 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/UniEats/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
